59eed6b6531b1ab41c7bbb8cb0c4a8eaec75e59d
baileyspace <bailey.r.smith@gmail.com>
2016-05-04 18:35:25 -0700

    Add set up params for PyPI

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from distutils.core import setup
 
 setup(
   name    = "audiosearch",
-  version = "1.0.0",
   packages = [ "Audiosearch" ],
+  version = "1.0.0",
+  description = 'Client to interact with the Audiosear.ch API',
+  author = "Pop Up Archive",
+  author_email = 'edison@popuparchive.com',
+  url = 'https://github.com/popuparchive/audiosearch-client-python',
+  download_url = 'https://github.com/popuparchive/audiosearch-client-python/tarball/1.0.0',
+  keywords = ['podcasts', 'audio', 'search'],
+  classifiers = [],
 )
